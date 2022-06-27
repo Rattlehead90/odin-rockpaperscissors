8eed933557735d9daf2e89c7fa14dfac717acdde
Rattlehead90 <strogonov@seznam.cz>
2022-06-27 13:16:54 +0200

    Add playRound() to play a round
    It works but win-lose statement lacks polishing as it returns only
    upper-cased forms of the option names (e.g. 'ROCK', 'PAPER').

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const options = ['Rock', 'Paper', 'Scissors']; //An array containing all possibl
 
 function computerPlay() {
     let randomlyGeneratedIndex = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min; //A randomly generated index to use with options array
-    console.log('The computer played: ' + options[randomlyGeneratedIndex(0, 2)]); //Fetching an option from the array, combining the randomlyGeneratedIndex and options
+    return options[randomlyGeneratedIndex(0, 2)].toUpperCase(); //Fetching an option from the array, combining the randomlyGeneratedIndex and options
 }
 
+function playRound(playerSelection, computerSelection) { 
+    playerSelection = prompt('Choose Rock, Paper or Scissors: ').toUpperCase();
+    computerSelection = computerPlay();
+
+    if ((playerSelection === 'PAPER' && computerSelection === 'ROCK') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') || (playerSelection === 'ROCK' && computerSelection === 'SCISSORS')) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if ((computerSelection === 'PAPER' && playerSelection === 'ROCK') || (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') || (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')){
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `It's a tie! ${computerSelection} and ${playerSelection}`;
+    }
+}
