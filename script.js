a3ca2b93c5b53648b199df795572d39145c36e67
Rattlehead90 <strogonov@seznam.cz>
2022-06-29 11:27:46 +0200

    Simplify computerPlay() as in main

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const options = ['Rock', 'Paper', 'Scissors']; //An array containing all possible options for computer to fetch from
 
-function computerPlay() {
-    let randomlyGeneratedIndex = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min; //A randomly generated index to use with options array
-    return options[randomlyGeneratedIndex(0, 2)].toUpperCase(); //Fetching an option from the array, combining the randomlyGeneratedIndex and options
+function computerPlay() { 
+    return options[Math.floor(Math.random() * (3))].toUpperCase(); 
 }
 
 
