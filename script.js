c79675e922e48ee7f2e3c5da5a1a093cfe14ce0d
Rattlehead90 <strogonov@seznam.cz>
2022-06-30 14:14:30 +0200

    Implement buttons' functionality module

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const options = ['Rock', 'Paper', 'Scissors']; //An array containing all possible options for computer to fetch from
 
 function computerPlay() { 
-    return options[Math.floor(Math.random() * (3))].toUpperCase(); 
+    computerSelection = options[Math.floor(Math.random() * (3))].toUpperCase(); 
+    return computerSelection;
 }
 
+const rockPlayer = document.querySelector('.rock-player');
+const paperPlayer = document.querySelector('.paper-player');
+const scissorsPlayer = document.querySelector('.scissors-player');
+const rockComputer = document.querySelector('.rock-computer');
+const paperComputer = document.querySelector('.paper-computer');
+const scissorsComputer = document.querySelector('.scissors-computer');
 
-function playRound(playerSelection, computerSelection) { 
+function playRound(playerSelection) { 
+    computerSelection = computerPlay();
     let playerSelectionToDisplay = playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase(); //Create a more polished version of an option name 
     let computerSelectionToDisplay = computerSelection.charAt(0) + computerSelection.slice(1).toLowerCase(); //Create a more polished version of an option name 
 
@@ -21,37 +29,50 @@ function playRound(playerSelection, computerSelection) {
             (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') || 
             (computerSelection === 'ROCK' && playerSelection === 'SCISSORS')
         ){ //Losing conditions for a player
-        console.log(`You lose! ${computerSelectionToDisplay} beats ${playerSelectionToDisplay}`);
+        console.log(`You lose! ${playerSelectionToDisplay} is beaten by ${playerSelectionToDisplay}`);
         return 'l' //returns a winLoseIndicator
     } else {
-        console.log(`It's a tie! ${computerSelectionToDisplay} and ${playerSelectionToDisplay}`);
+        console.log(`It's a tie! ${playerSelectionToDisplay} and ${playerSelectionToDisplay}`);
         return 't' //returns a winLoseIndicator
     }
 }
 
-function game() {
-    let playerScore = 0; //Create score counters
-    let computerScore = 0;
-    let winLoseIndicator = ''; //Instantiate a winLoseIndicator
+// function game() {
+//     let playerScore = 0; //Create score counters
+//     let computerScore = 0;
+//     let winLoseIndicator = ''; //Instantiate a winLoseIndicator
 
-    while ((playerScore < 5) && (computerScore < 5)) {
-        let playerSelection = prompt('Choose Rock, Paper or Scissors: ').toUpperCase(); //Ask for an input
-        let computerSelection = computerPlay();
-        winLoseIndicator = playRound(playerSelection, computerSelection);
-        if (winLoseIndicator === 'w') {
-            playerScore++;
-            console.log('The score is: ' + playerScore + ' : ' + computerScore);
-        } else if (winLoseIndicator === 'l') {
-            computerScore++;
-            console.log('The score is: ' + playerScore + ' : ' + computerScore);
-        } else {
-            console.log('The score is: ' + playerScore + ' : ' + computerScore);
-        }
-    }
+//     while ((playerScore < 5) && (computerScore < 5)) {
+//         let playerSelection = prompt('Choose Rock, Paper or Scissors: ').toUpperCase(); //Ask for an input
+//         let computerSelection = computerPlay();
+//         winLoseIndicator = playRound(playerSelection, computerSelection);
+//         if (winLoseIndicator === 'w') {
+//             playerScore++;
+//             console.log('The score is: ' + playerScore + ' : ' + computerScore);
+//         } else if (winLoseIndicator === 'l') {
+//             computerScore++;
+//             console.log('The score is: ' + playerScore + ' : ' + computerScore);
+//         } else {
+//             console.log('The score is: ' + playerScore + ' : ' + computerScore);
+//         }
+//     }
 
-    if (playerScore > computerScore) {
-        return 'Congratulations! You\'ve won!'
-    } else {
-        return 'Computer wins!'
-    }
-}
+//     if (playerScore > computerScore) {
+//         return 'Congratulations! You\'ve won!'
+//     } else {
+//         return 'Computer wins!'
+//     }
+// }
+
+//Buttons' functionality 
+rockPlayer.addEventListener('click', () => {
+    playRound('ROCK');
+});
+
+paperPlayer.addEventListener('click', () => {
+    playRound('PAPER');
+})
+
+scissorsPlayer.addEventListener('click', () => {
+    playRound('SCISSORS');
+})
